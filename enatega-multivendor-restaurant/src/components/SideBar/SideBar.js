aff5a8f873cdd88128aacf98e0abc733844786fc
Deivis Torres Mena <babyyoda62406@gmail.com>
2025-04-08 10:17:41 -0400

    fix: Restaurant

--- enatega-multivendor-restaurant/src/components/SideBar/SideBar.js
+++ enatega-multivendor-restaurant/src/components/SideBar/SideBar.js
@@ -2,22 +2,19 @@ import React, { useState, useEffect, useRef } from 'react'
 import {
   View,
   ImageBackground,
-  Platform,
-  Switch,
+  // Platform,
   ActivityIndicator,
   TouchableOpacity,
   Linking,
   AppState
 } from 'react-native'
 import TextDefault from '../Text/TextDefault/TextDefault'
-import { colors } from '../../utilities/colors'
 import bg from '../../assets/restBackground.png'
 import styles from './styles'
 import { Icon } from 'react-native-elements/dist/icons/Icon'
 import { useAccount } from '../../ui/hooks'
 import { Image } from 'react-native-elements'
 import useNotification from '../../ui/hooks/useNotification'
-import { PRODUCT_URL, ABOUT_URL } from '../../utilities'
 import { useTranslation } from 'react-i18next'
 import Constants from 'expo-constants'
 
@@ -25,7 +22,7 @@ export default function SideBar() {
   const { t } = useTranslation()
   const notificationRef = useRef(true)
   const openSettingsRef = useRef(false)
-  const { logout, data, toggleSwitch, isAvailable } = useAccount()
+  const { logout, data } = useAccount()
   const [notificationStatus, setNotificationStatus] = useState(false)
   const appState = useRef(AppState.currentState)
 
@@ -33,7 +30,7 @@ export default function SideBar() {
     restaurantData,
     getPermission,
     getExpoPushToken,
-    requestPermission,
+    // requestPermission,
     sendTokenToBackend
   } = useNotification()
 
@@ -85,31 +82,32 @@ export default function SideBar() {
     }
   }, [])
 
-  const handleClick = async () => {
-    if (notificationStatus === false) {
-      const permissionStatus = await getPermission()
-      if (permissionStatus.granted) {
-        setNotificationStatus(true)
-        const token = (await getExpoPushToken({ projectId: Constants.expoConfig.extra.eas.projectId })).data
-        sendTokenToBackend({ variables: { token, isEnabled: true } })
-      } else if (permissionStatus.canAskAgain) {
-        const result = await requestPermission()
-        if (result.granted) {
-          setNotificationStatus(true)
-          const token = (await getExpoPushToken({ projectId: Constants.expoConfig.extra.eas.projectId })).data
-          sendTokenToBackend({ variables: { token, isEnabled: true } })
-        }
-      } else {
-        openSettingsRef.current = true
-        Platform.OS === 'ios'
-          ? Linking.openURL('app-settings:')
-          : Linking.openSettings()
-      }
-    } else {
-      setNotificationStatus(false)
-      sendTokenToBackend({ variables: { token: null, isEnabled: false } })
-    }
-  }
+  // Disclaimer: DTM.
+  // const handleClick = async () => {
+  //   if (notificationStatus === false) {
+  //     const permissionStatus = await getPermission()
+  //     if (permissionStatus.granted) {
+  //       setNotificationStatus(true)
+  //       const token = (await getExpoPushToken({ projectId: Constants.expoConfig.extra.eas.projectId })).data
+  //       sendTokenToBackend({ variables: { token, isEnabled: true } })
+  //     } else if (permissionStatus.canAskAgain) {
+  //       const result = await requestPermission()
+  //       if (result.granted) {
+  //         setNotificationStatus(true)
+  //         const token = (await getExpoPushToken({ projectId: Constants.expoConfig.extra.eas.projectId })).data
+  //         sendTokenToBackend({ variables: { token, isEnabled: true } })
+  //       }
+  //     } else {
+  //       openSettingsRef.current = true
+  //       Platform.OS === 'ios'
+  //         ? Linking.openURL('app-settings:')
+  //         : Linking.openSettings()
+  //     }
+  //   } else {
+  //     setNotificationStatus(false)
+  //     sendTokenToBackend({ variables: { token: null, isEnabled: false } })
+  //   }
+  // }
 
   return (
     <View style={{ flex: 1 }}>
@@ -137,79 +135,15 @@ export default function SideBar() {
           </View>
         </View>
         <View style={styles.middleContainer}>
-          <View style={styles.status}>
-            <TextDefault H4 bolder textColor="white">
-              {t('status')}
-            </TextDefault>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <TextDefault textColor="white" style={{ marginRight: 5 }}>
-                {isAvailable ? t('online') : t('closed')}
-              </TextDefault>
-              <Switch
-                trackColor={{
-                  false: colors.fontSecondColor,
-                  true: colors.green
-                }}
-                thumbColor={isAvailable ? colors.headerBackground : '#f4f3f4'}
-                ios_backgroundColor="#3e3e3e"
-                onValueChange={toggleSwitch}
-                value={isAvailable}
-                style={{ marginTop: Platform.OS === 'android' ? -15 : -5 }}
-              />
-            </View>
-          </View>
-          <View style={styles.status}>
-            <TextDefault H4 bolder textColor="white">
-              {t('notifications')}
-            </TextDefault>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <TextDefault textColor="white" style={{ marginRight: 5 }}>
-                {notificationStatus ? t('onn') : t('off')}
-              </TextDefault>
-              <Switch
-                trackColor={{
-                  false: colors.fontSecondColor,
-                  true: colors.green
-                }}
-                thumbColor={
-                  notificationStatus ? colors.headerBackground : '#f4f3f4'
-                }
-                ios_backgroundColor="#3e3e3e"
-                onValueChange={handleClick}
-                value={notificationStatus}
-                style={{ marginTop: Platform.OS === 'android' ? -15 : -5 }}
-              />
-            </View>
-          </View>
-          <TouchableOpacity
-            style={styles.logout}
-            activeOpacity={0.8}
-            onPress={() =>
-              Linking.canOpenURL(PRODUCT_URL).then(() => {
-                Linking.openURL(PRODUCT_URL)
-              })
-            }>
-            <View style={styles.icon}>
-              <Icon
-                type="font-awesome"
-                color="white"
-                name="product-hunt"
-                size={26}
-              />
-            </View>
-            <TextDefault H4 bolder style={styles.text}>
-              {t('productPage')}
-            </TextDefault>
-          </TouchableOpacity>
           <TouchableOpacity
             style={styles.logout}
             activeOpacity={0.8}
             onPress={() =>
               Linking.canOpenURL(
-                'https://enatega.com/privacy-policy/'
+                'https://dinospedidos.com/#/privacy'
               ).then(() => {
                 Linking.openURL(
-                  'https://enatega.com/privacy-policy/'
+                  'https://dinospedidos.com/#/privacy'
                 )
               })
             }>
@@ -225,8 +159,12 @@ export default function SideBar() {
             style={styles.logout}
             activeOpacity={0.8}
             onPress={() =>
-              Linking.canOpenURL(ABOUT_URL).then(() => {
-                Linking.openURL(ABOUT_URL)
+              Linking.canOpenURL(
+                'https://dinospedidos.com'
+              ).then(() => {
+                Linking.openURL(
+                  'https://dinospedidos.com/'
+                )
               })
             }>
             <View style={styles.icon}>
